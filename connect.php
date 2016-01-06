7e9f0f67c4d78a999f79a722671ca1a3010d41ed
Fedor Petryk <fedor.p@techery.io>
2016-01-06 14:48:18 +0200

    Improve of .env.sample

--- connect.php
+++ connect.php
@@ -6,12 +6,12 @@ use PhpOrient\Protocols\Binary\Data\Record;
 
 Dotenv::load(__DIR__);
 
-$dbName = getenv('DB_DATABASE')?: 'Users';
+$dbName = getenv('DB_DATABASE') ?: 'GratefulDeadConcerts';
 
 $config = [
     'username' => getenv('DB_USERNAME') ?: 'root',
     'password' => getenv('DB_PASSWORD') ?: '0r13ntDB',
-    'hostname' => getenv('DB_HOST') ?: '172.17.42.1',
+    'hostname' => getenv('DB_HOST') ?: 'localhost',
     'port'     => getenv('DB_PORT') ?: 2424,
 ];
 
@@ -25,18 +25,20 @@ if (!$client->dbExists($dbName)) {
 
 $client->dbOpen($dbName, $config['username'], $config['password']);
 
+$userClassName = getenv('DB_USER_CLASS') ?: 'User';
+
 try {
-    $client->command('create class Users extends V');
+    $client->command("create class {$userClassName}");
 } catch (Exception $e) {
-    echo 'class Users already creates';
+    echo 'class Users already exists', PHP_EOL;
 }
 
 $userName = mt_rand(1000, 100000);
 $userType = 'Guest';
 
-$client->command("insert into {$dbName} set name = '{$userName}', type = '{$userType}'");
+$client->command("insert into {$userClassName} set name = '{$userName}', type = '{$userType}'");
 
-$users = $client->query('select from Users');
+$users = $client->query("select from {$dbName}");
 
 /** @var  Record $user */
 foreach ($users as $user) {
